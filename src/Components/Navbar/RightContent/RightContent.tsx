14c32b9fdac8124f84120830f106413cfc999f0f
surajsharma <surajsharma@cybrilla.com>
2022-05-10 15:36:14 +0530

    navbar user menu

--- src/Components/Navbar/RightContent/RightContent.tsx
+++ src/Components/Navbar/RightContent/RightContent.tsx
@@ -1,9 +1,11 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, Menu } from "@chakra-ui/react";
 import { signOut } from "firebase/auth";
 import React from "react";
 import { auth } from "../../../firebase/clientApp";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
+import UserMenu from "./UserMenu";
 
 type RightContentProps = {
     user: any;
@@ -14,11 +16,8 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
         <>
             <AuthModal />
             <Flex justify={"center"} align={"center"}>
-                {user ? (
-                    <Button onClick={() => signOut(auth)}>Logout</Button>
-                ) : (
-                    <AuthButtons />
-                )}
+                {user ? <Icons /> : <AuthButtons />}
+                <UserMenu user={user} />
             </Flex>
         </>
     );
